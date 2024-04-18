bc76f5323cc4a2e92bbe6ab65e28af983c20d354
hanzug <1373338025@qq.com>
2024-04-18 22:10:37 +0800

    [update] designPattern

--- designPattern/singleton/lazy/lazy.go
+++ designPattern/singleton/lazy/lazy.go
@@ -13,8 +13,10 @@ var mu sync.Mutex
 // GetInstance 双重检查加锁的懒汉模式
 func GetInstanceLazy() *Singleton {
 	if instance == nil {
+		//这里会出现两个线程同时进入的情况。然后他们就会竞争锁。
 		mu.Lock()
 		defer mu.Unlock()
+		//得到锁的线程再次进行判断，防止多次创建。
 		if instance == nil {
 			instance = &Singleton{}
 		}
